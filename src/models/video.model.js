8e039686528d2a3bcece15495a0a31903f3434f6
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-10 17:32:21 +0530

    Add video model file, mongoose aggregate plugin

---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"; //?plugin as middleware. for more powerfull queries.
+///Docs : https://github.com/aravindnc/mongoose-aggregate-paginate-v2#readme //TODO: mongoDB aggregate pipelines
+///docs : https://mongoosejs.com/docs/middleware.html
+
+///Docs : https://mongoosejs.com/docs/schematypes.html
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail:{
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description:{
+      type: String,
+      required: true,
+    },
+    duration:{
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0
+    },
+    isPublished: {
+      type: Boolean,
+      default: true
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = Model("Video", videoSchema);
