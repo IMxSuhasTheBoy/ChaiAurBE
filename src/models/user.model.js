c76f2e6ba95319269ecac03b068356070aa79be7
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-10 17:30:38 +0530

    Add user model file,  jwt & bcrypt methods

---
+++ src/models/user.model.js
@@ -0,0 +1,114 @@
+import mongoose, { Schema, Model } from "mongoose";
+import jwt from "jsonwebtoken"; //* JWT is a bearer token (refrence a key of whose whoever owns it).
+//Docs : https://github.com/auth0/node-jsonwebtoken#readme
+
+import bcrypt from "bcrypt";
+
+///Docs : https://mongoosejs.com/docs/schematypes.html
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+
+
+//TODO: only exicute the middleware on password changes, Is to be taken care of.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+
+
+//---------------------- Additional methods in schema object ---------------------- 
+//TODO: To compare (PW received in user req.) with the (recent saved PW).
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+//! returns boolean result.
+
+
+
+//TODO: To generate access token & refresh token, With JWT.
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  )
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  )
+};
+//---------------------- Additional methods in schema object-----------------------
+
+
+
+
+
+
+
+export const User = mongoose.model("User", userSchema);
