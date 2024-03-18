5bd4207004dd2dd6e458b1ecdc6d932d8a09f318
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-18 22:10:32 +0530

    Add avatar route for api/v1/users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,6 +7,7 @@ import {
   refreshAccessToken,
   registerUser,
   updateAccountDetails,
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -39,5 +40,9 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
 export default router;
