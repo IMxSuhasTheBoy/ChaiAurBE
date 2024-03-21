4afc58ec6c673c8058aa4a8cd11227e6cfb2cd19
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-21 12:33:19 +0530

    Add util function destroyFileOnCloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import { extractPublicId } from "cloudinary-build-url";
 import fs from "fs";
 
 cloudinary.config({
@@ -16,21 +17,35 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto", //*Additional options as per requirements.
     });
     //*file has been uploaded successfully,
-    
-    // ? console.log(`File is uploaded on cloudinary! ! ! ${response.url} ! ! !`);
-    //Examin console.log("22", response);
 
-    
+    // ? console.log(`File is uploaded on cloudinary! ! ! ${response.url} ! ! !`);
+    console.log("Upload cloudinary respose", response.url);
+
     //TODO: Remove the locally saved temporary file after confirming the uploaded file path with local path of file.
     fs.unlinkSync(localFilePath);
 
     //TODO: file upload success so return the response;
     return response;
-
   } catch (error) {
     fs.unlinkSync(localFilePath); ///Remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 };
 
-export { uploadOnCloudinary };
+const destroyFileOnCloudinary = async (oldFileUrl) => {
+  try {
+    //TODO: extract publicId from oldFileUrl
+    // const publicId = oldFileUrl.match(/\/v\d+\/([^\.\/]+)/)[1];
+    const publicId = extractPublicId(oldFileUrl);
+    if (!publicId) return null;
+
+    //TODO: destroy file on cloudinary
+    const response = await cloudinary.uploader.destroy(publicId);
+
+    //TODO: file destroy success so return the response;
+    return response;
+  } catch (error) {
+    return null;
+  }
+};
+export { uploadOnCloudinary, destroyFileOnCloudinary };
