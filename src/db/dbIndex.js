d117d8df5e26d18f3ff10c3781192e115a703239
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-22 16:25:59 +0530

    Add checks for edge cases handling

--- src/db/dbIndex.js
+++ src/db/dbIndex.js
@@ -3,11 +3,35 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
+  if (!process.env.MONGODB_URI) {
+    console.error(
+      "MongoDB connection Failed! ! ! MONGODB_URI not found in env"
+    );
+    process.exit(1);
+  }
+  if (!DB_NAME) {
+    console.error("MongoDB connection Failed! ! ! DB_NAME not found");
+    process.exit(1);
+  }
+
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`\nMongoDB connected ! ! ! DB HOST: ${connectionInstance.connection.host}`);
-  } catch (err) {
-    console.log("MongoDB connection Failed!!!", err);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    if (!connectionInstance || !connectionInstance.connection) {
+      throw new Error(
+        "MongoDB connection Failed! ! ! connectionInstance is null\n"
+      );
+    }
+    console.log(
+      `\nMongoDB connected ! ! ! DB HOST: ${connectionInstance.connection.host}\n`
+    );
+  } catch (error) {
+    console.error(`MongoDB connection Failed! ! ! : ${error}\n`);
+
+    if (!error) {
+      console.error("MongoDB connection Failed! ! ! Unknown Error\n");
+    }
     process.exit(1);
   }
 };
