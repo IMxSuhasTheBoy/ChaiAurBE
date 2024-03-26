3045675a9629d3650200417420def261f7c3b6e6
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-26 22:25:42 +0530

    Add jsDoc, refactored

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,23 +1,37 @@
-/// 2.approach, returning the function in promises format
+/**
+ * 2.approach HOF: returning the function in promises format
+ * @param {function} reqHandler - The function to be wrapped, invoked as Promise (will be a asynchronous request handler function)
+ * @returns {function} - The wrapped asynchronous request handler function.
+ */
 const asyncHandler = (reqHandler) => {
   return (req, res, next) => {
-    Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err));
+    Promise.resolve(reqHandler(req, res, next)).catch((error) => next(error));
   };
 };
 export { asyncHandler };
 
-/// 1.approach HOF: const asyncHandler = (Fn) => { async () => {trycatch} }; /// accepting a func then passing it into an func
+/** 1.approach HOF: const asyncHandler = (Fn) => { async () => {trycatch} };
+ * accepting a func
+ * then passing it into an function that takes
+ * the req, res, next params extracted from Fn
+ * and It then returns a new function that handles asynchronous operations by calling the original function Fn  * with the req, res, and next parameters, and includes error handling.
+ * If an error occurs, it sets the response status to the error code or 500 and sends a JSON response with the  * error message.
+ *
+ * @param {Function} Fn - The function to be wrapped. (will be a asynchronous request handler function)
+ * @returns {Function} - The wrapped function.
+ */
 /*
 const asyncHandler = (Fn) => async (req, res, next) => {
   try {
     await Fn(req, res, next);
   } catch (error) {
-    ///expecting error code from user
-    res.status(err.code || 500).json({
+    // expecting error code from user
+    res.status(error.code || 500).json({
       success: false,
-      message: err.message,
+      message: error.message,
     });
   }
 };
 */
-///pourpose/output : take a func wrap it with try catch (for error handling work) then return the func
+
+//* purpose handle error in standardised format/outcome : take a func wrap it Error handling code.
