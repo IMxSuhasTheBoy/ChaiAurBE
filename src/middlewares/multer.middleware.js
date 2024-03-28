a2c02ad99f98e1f72b47af76ac1ec78f23cdb89d
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-28 23:43:54 +0530

    Add comments for code explanation

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -7,12 +7,24 @@ const storage = multer.diskStorage({
   },
 
   filename: function (req, file, cb) {
-    cb(null, file.originalname); //! configure as per requirement
+    cb(null, file.originalname); //! configure as per requirement, may multiple files of same name can be uploaded (will be overriden)
 
-    console.log(file.fieldname, file.originalname, "Multer");
+    console.log(file, "=====", file.fieldname, file.originalname, "Multer");
   },
 });
 
 export const upload = multer({
   storage,
 });
+/*
+* Multer Configuration:
+The code sets up Multer disk storage configuration for handling file uploads in an Express application.
+* Disk Storage Configuration:
+The storage constant defines the disk storage settings for Multer, specifying the destination and filename functions for uploaded files.
+* Destination Function:
+The destination function determines where uploaded files will be stored on the server. In this case, files are saved in the "./public/temp" directory.
+* Filename Function:
+The filename function specifies how uploaded files will be named. It uses the original filename of the uploaded file.
+* Multer Middleware:
+The upload constant creates a Multer middleware instance using the configured storage settings, allowing the application to handle file uploads based on the defined storage configuration.
+*/
