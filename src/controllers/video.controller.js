c006fe61d7ddbc2f5249938d281ff80c138eeb00
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-04-06 21:51:27 +0530

    Add file, controller getAllVideos for videos route

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,163 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {
+  destroyFileOnCloudinary,
+  uploadOnCloudinary,
+} from "../utils/cloudinary.js";
+
+//TODO: Get all videos based on query, sort, pagination
+const getAllVideos = asyncHandler(async (req, res) => {
+  let { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+
+  //TODO: 1 Parse page and limit to numbers. Base 10 (decimal): The default base if the radix is not provided. Numbers are represented using digits 0-9.
+  page = parseInt(page, 10);
+  limit = parseInt(limit, 10);
+
+  //TODO: 2 Set default values if query params are not provided
+  page = Math.max(1, page); // page should be greater than 0 //ex: page is -1, it will be set to 1 / page is 3, it will be set to 3
+  limit = Math.min(20, Math.max(1, limit)); // limit should be between 1 and 20 //ex: limit is 0, it will be set to 1 / limit is 50, it will be set to 20
+
+  const pipeline = []; //TODO: 3 - Structure pipeline based on below checks & query params
+
+  // match videos by owner userId if provided
+  if (userId) {
+    if (!isValidObjectId(userId)) {
+      throw new ApiError(400, "Invalid userId");
+    }
+
+    pipeline.push({
+      $match: {
+        videoOwner: new mongoose.Types.ObjectId(userId),
+      },
+    });
+  } //ex: [{'$match': { videoOwner: new ObjectId('660d44370718ff71f2fd8163') }},{ '$sort': { createdAt: -1 } },{ '$skip': 0 },{ '$limit': 10 }]
+
+  // match videos by query if provided
+  if (query && query.trim() !== "") {
+    pipeline.push({
+      $match: {
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { description: { $regex: query, $options: "i" } },
+        ], //looks for substring present in title/description field of the document case-insensessive operation
+      },
+    });
+  }
+
+  // sort videos by sortBy and sortType if provided
+  const sortCriteria = {};
+  if (sortBy && sortType) {
+    sortCriteria[sortBy] = sortType === "asc" ? 1 : -1;
+    pipeline.push({
+      $sort: sortCriteria,
+    });
+  } else {
+    // default sort by createdAt in descending order
+    sortCriteria["createdAt"] = -1;
+    pipeline.push({
+      $sort: sortCriteria,
+    });
+  }
+
+  // add pagination using skip and limit
+  pipeline.push({
+    $skip: (page - 1) * limit,
+  });
+  pipeline.push({
+    $limit: limit,
+  });
+
+  // console.log("pipeline", pipeline, "pipeline");
+
+  /** // execute the aggregatePaginate pipeline returns { "statusCode": 200, "data": { "docs":[ {}, {}, {} ] }, "details":  .... }
+  Video.aggregatePaginate(pipeline)
+    .then((result) => {
+      return res
+        .status(200)
+        .json(new ApiResponse(200, result, "Videos fetched successfully!!!"));
+    })
+    .catch((error) => {
+      throw new ApiError(
+        500,
+        error?.message || "Error in fetching videos! ! !"
+      );
+    });
+  */
+  //TODO: 4 execute the aggregation pipeline
+  try {
+    const videos = await Video.aggregate(pipeline).exec();
+    console.log(videos, " : videos");
+
+    if (!videos) {
+      throw new ApiError(500, "Error in fetching videos! ! !");
+    }
+
+    //TODO: 5
+    return res
+      .status(200)
+      .json(new ApiResponse(200, videos, "Videos fetched successfully!!!"));
+  } catch (error) {
+    throw new ApiError(500, error?.message || "Error in fetching videos! ! !");
+  }
+});
+
+
+export {
+  getAllVideos,
+ 
+};
+
+//!Experimental
+/** async function uploadWithRetry(file) {
+ try {
+   const result = await uploadToCloudinary(file);
+   return result;
+  } catch (error) {
+    console.error('Upload failed, retrying...');
+    return uploadWithRetry(file); // Recursive call to retry upload
+  }
+}
+
+async function uploadToCloudinary(file) {
+  // Code to upload file to Cloudinary asynchronously
+}
+
+// To initiate the upload operation with retry
+const file = 'your_file_to_upload';
+uploadWithRetry(file)
+.then(result => {
+  console.log('Upload successful:', result);
+})
+.catch(error => {
+  console.error('Max retries reached, upload failed:', error);
+});
+*/
+
+//---------------------------------------------------------
+/* 
+//TODO: 3 Files upload on cloud operations
+const videoFile = await uploadOnCloudinary(
+  videoFileLocalPath,
+  "videos/videoFile"
+);
+// let thumbnailFile = undefined;
+// if (!videoFile && videoFile?.url === "") {
+  //   throw new ApiError(
+    //     500,
+    //     "Something went wrong while uploading videoFile! ! !"
+    //   );
+    // } else {
+      //   console.log("video upload successfully!!!");
+      //   thumbnailFile = await uploadOnCloudinary(
+        //     thumbnailFileLocalPath,
+        //     "videos/thumbnailFile"
+        //   );
+        // } //? make thumbnail upload operation repeats/retries itself till succeedes || some tries 
+        // Strategy for error (both required case) : When videoFile succeeds ? try thumbnailFile : error, when thumbnailFile succeeds ? move to video creation : error✔. || destroy succeded one if any one fails (repeat till both succeedes to move create video) || keep succeded one at cloud till both succeed via retry by client, if client fails/exits/cancels process to publish-> then destroy succeded one. || try upload thumbnail after video created && optionaly upload a temporary thumbnail*
+        // // or make thumbnailFile optional if not provided. || not required
+        */
+
+//!Experimental
