3b9d716de9d0b9d6bb1e6cd477c43705f9f08350
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-04-08 23:33:19 +0530

    Import & declare subscriptionRouter routes

--- src/app.js
+++ src/app.js
@@ -22,10 +22,12 @@ app.use(cookieParser()); ///Github https://github.com/expressjs/cookie-parser#re
 import userRouter from "./routes/user.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 //TODO: routes declaration
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export { app };
