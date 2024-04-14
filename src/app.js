5f88d9b980a68e7d9a21ab9f51263dc1db8e1d83
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-04-14 19:38:44 +0530

    Import & declare commentRouter routes

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import communityPostRouter from "./routes/communityPost.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+
 
 //TODO: routes declaration
 app.use("/api/v1/healthcheck", healthcheckRouter);
@@ -31,5 +33,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/communityPosts", communityPostRouter);
+app.use("/api/v1/comments", commentRouter);
+
 
 export { app };
