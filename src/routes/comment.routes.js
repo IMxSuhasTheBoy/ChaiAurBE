e265c19362772e379eba861f3403d7705c034c6e
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-04-16 23:43:47 +0530

    Add new controllers

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {
-  addComment,
-  deleteComment,
+  addCommentOnVideo,
+  addCommentOnCommunityPost,
   getVideoComments,
+  getCommunityPostComments,
   updateComment,
+  deleteComment,
 } from "../controllers/comment.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -11,7 +13,12 @@ const router = Router();
 
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/:documentId").get(getVideoComments).post(addComment);
+router.route("/vid/:videoId").get(getVideoComments).post(addCommentOnVideo);
+
+router
+  .route("/cp/:communityPostId")
+  .get(getCommunityPostComments)
+  .post(addCommentOnCommunityPost);
 router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
 
 export default router;
