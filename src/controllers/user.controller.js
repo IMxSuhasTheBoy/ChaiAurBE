8248450f647a450f4d06ca0134d952954f87f21f
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-04-20 23:13:52 +0530

    Refactored user operations

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,14 @@
-import { User } from "../models/user.model.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import {
   destroyFileOnCloudinary,
   uploadOnCloudinary,
 } from "../utils/cloudinary.js";
-import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
+
+import { User } from "../models/user.model.js";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   /**Generate access and refresh tokens for a user.
@@ -38,47 +39,45 @@ const generateAccessAndRefreshTokens = async (userId) => {
   }
 };
 
+//TODO: The fn creates new user document
 const registerUser = asyncHandler(async (req, res) => {
   /* test res.status(200).json({
     message: "Chai Aur Code",
   });*/
 
-  //TODO: 1
+  //TODO: 1 take fields
   const { fullName, email, username, password } = req.body; //TODO: apply checks for valid email, password on frontend preferably
   // console.log("registerUser", req.body);
 
-  //TODO: 2
+  //TODO: 2 check for empty & valid fields
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
-  ) {
+  )
     throw new ApiError(400, "All fields are required! ! !");
-  }
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!email.match(emailRegex)) {
+  if (!email.match(emailRegex))
     throw new ApiError(400, "Invalid email address! ! !");
-  }
 
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
-  if (!passwordRegex.test(password)) {
+  if (!passwordRegex.test(password))
     throw new ApiError(
       422,
       "Password does not meet the criteria. Please use a password with at least 8 characters, including uppercase, lowercase, numbers, and special characters! ! !"
     );
-  }
 
-  //TODO: 3
-  const existeduser = await User.findOne({
+  //TODO: 3 check for existing user
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
-  if (existeduser) {
+
+  if (existedUser)
     throw new ApiError(409, "User with email or username already exists! ! !");
-  }
 
   // console.log("registerUser TODO: 4", req.files);
-  //TODO: 4.1
+  //TODO: 4.1 check for files
   //*access of files is from multer middleware.
-  const avatarLocalPath = req.files?.avatar[0]?.path; //*Expecting: avatar is present, means- found the edge case to cover now with non required fields.
+  const avatarLocalPath = req.files?.avatar?.[0]?.path; //*Expecting: avatar is present, means- found the edge case to cover now with non required fields.
   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
   //*above edge case covered
@@ -92,15 +91,15 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   //TODO: 4.2
-  if (!avatarLocalPath) {
+  if (!avatarLocalPath)
     throw new ApiError(
       400,
       "Avatar file is required!!! not stored in local! ! !"
     );
-  }
 
-  //TODO: 4.3
+  //TODO: 4.3 upload files & check for success
   const avatar = await uploadOnCloudinary(avatarLocalPath, "users/avatar");
+
   const coverImage = await uploadOnCloudinary(
     coverImageLocalPath,
     "users/coverImage"
@@ -108,14 +107,13 @@ const registerUser = asyncHandler(async (req, res) => {
   // console.log("TODO: 4.3", avatar, coverImage);
 
   //TODO: 4.4
-  if (!avatar) {
+  if (!avatar)
     throw new ApiError(
       400,
       "Avatar file is missing!!! not uploaded in cloud! ! !"
     );
-  }
 
-  //TODO: 5.1
+  //TODO: 5.1 create user
   const user = await User.create({
     fullName,
     avatar: avatar.url,
@@ -132,13 +130,12 @@ const registerUser = asyncHandler(async (req, res) => {
   );
   //? at the moment of user creation refreshToken isn't need to create?
 
-  //TODO: 7.1
-  if (!createdUser) {
+  //TODO: 7.1 check for success
+  if (!createdUser)
     throw new ApiError(
       500,
       "Something went wrong while registering the user! ! !"
     );
-  }
 
   //TODO: 7.2
   return res
@@ -146,86 +143,90 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully!!!"));
 });
 
+//TODO: The fn logins user with requested credentials
 const loginUser = asyncHandler(async (req, res) => {
-  //TODO: 1
+  //TODO: 1 take fields
   const { email, username, password } = req.body;
 
-  //TODO: 2
-  if (!username && !email) {
-    throw new ApiError(400, "Username and Email is reqired! ! !");
-  }
-  // Here is an alternative of above code based on logic :
-  // if (!(username || email)) {
-  //     throw new ApiError(400, "username or email is required")
-  // }
+  //TODO: 2 check for empty & valid fields
+  const isFieldEmpty = (field) => !field || field.trim() === "";
 
-  //TODO: 3
+  if (isFieldEmpty(username) && isFieldEmpty(email))
+    throw new ApiError(
+      400,
+      "Username and Email is reqired and should not be empty! ! !"
+    );
+  // Here is an alternative of above code based on logic :
+  // if (!(username || email))
+  //     throw new ApiError(400, "username or email is required")
+
+  //TODO: 3 check for existing user
   const user = await User.findOne({
     $or: [{ username }, { email }],
   });
   //? Here is an alternative of above code based on logic : what if an registered email is associated with more than 1 username/vise versa ?
   // const user = await User.findOne({ username, email });
 
-  if (!user) {
-    throw new ApiError(404, "User does not exist! ! !");
-  }
+  if (!user) throw new ApiError(404, "User does not exist! ! !");
 
-  //TODO: 4
-  //*bcrypt: additional methods in schema Object instance, user is our instance of User Schema
+  //TODO: 4 check for password
+  //*bcrypt: additional methods of User schema Object instance
   const isPasswordValid = await user.isPasswordCorrect(password);
 
-  if (!isPasswordValid) {
+  if (!isPasswordValid)
     throw new ApiError(401, "Invalid user credentials! ! !");
-  }
 
-  //TODO: 5
+  //TODO: 5 generate tokens & save refresh token in user document
   const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
     user._id
   );
 
   //TODO: 6.1 strategy for auth & session management:
   //? update variable of user object with accessToken / Have variable of user instance From DB with unnecessary fields ommited
-
-  const loggedInUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  );
-
-  //console.log("loggedInUser", loggedInUser);
-
-  //console.log(".env file options", process.env.HTTP_ONLY === "true"); //*For both true and false boolean values in .env, It will be treated as a string and when compared to 'true', it will evaluate to false/true respectively for both true and false values.
-
-  //TODO: 6.2
-  const options = {
-    httpOnly: process.env.HTTP_ONLY === "true",
-    secure: process.env.SECURE === "true",
-  };
-
-  return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json(
-      new ApiResponse(
-        200,
-        {
-          user: loggedInUser,
-          accessToken,
-          refreshToken,
-        },
-        "User logged In Successfully!!!"
-      )
+  try {
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
     );
-  //sending accessToken and refreshToken in json response (data). Should be as per strategy requirement
+
+    //console.log("loggedInUser", loggedInUser);
+
+    //console.log(".env file options", process.env.HTTP_ONLY === "true"); //*For both true and false boolean values in .env, It will be treated as a string and when compared to 'true', it will evaluate to false/true respectively for both true and false values.
+
+    //TODO: 6.2 cookies for auth & session management
+    const options = {
+      httpOnly: process.env.HTTP_ONLY === "true",
+      secure: process.env.SECURE === "true",
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          {
+            user: loggedInUser,
+            accessToken,
+            refreshToken,
+          },
+          "User logged In Successfully!!!"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(500, error?.message || "Failed to logging in user! ! !");
+  }
+  //!sending accessToken and refreshToken in json response (data). Should be as per strategy requirement
 });
 
+//TODO: The fn logs out the currently logged in user
 const logoutUser = asyncHandler(async (req, res) => {
   //console.log("logoutUser verifiedJWT", req.user._id);
+  // console.log("logout", req, "logout");
 
-  console.log("logout", req, "logout");
-
-  //TODO: 2.1 2.2
+  //TODO: 2.1 2.2 find and remove field refreshToken from user document
   await User.findByIdAndUpdate(
-    req.user._id,
+    req.user?._id,
     {
       $unset: {
         refreshToken: 1, // this removes the field from document
@@ -236,7 +237,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     }
   );
 
-  //TODO: 2.3
+  //TODO: 2.3 clear cookies
   const options = {
     httpOnly: process.env.HTTP_ONLY === "true",
     secure: process.env.SECURE === "true",
@@ -249,36 +250,33 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged Out!!!"));
 });
 
+//TODO: The fn refreshes access token when, everytime user log in, when cookie refresh token have valid validity
 const refreshAccessToken = asyncHandler(async (req, res) => {
-  //TODO: 1
+  //TODO: 1 check refresh token
   const incommingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
-  if (!incommingRefreshToken) {
+  if (!incommingRefreshToken)
     throw new ApiError(401, "Unauthorized request! ! !");
-  }
 
   try {
-    //TODO: 2
+    //TODO: 2 decode with secret key
     const decodedToken = jwt.verify(
       incommingRefreshToken,
       process.env.REFRESH_TOKEN_SECRET
     );
     // console.log("T2 decodedToken", decodedToken);
 
-    //TODO: 3
+    //TODO: 3 find user by matching refresh token
     const user = await User.findById(decodedToken?._id);
 
-    if (!user) {
-      throw new ApiError("Invalid refresh Token! ! !");
-    }
+    if (!user) throw new ApiError("Invalid refresh Token! ! !");
 
-    //TODO: 4
-    if (incommingRefreshToken !== user?.refreshToken) {
+    //TODO: 4 verify refresh token
+    if (incommingRefreshToken !== user?.refreshToken)
       throw new ApiError(401, "Refresh token is expired or used! ! !");
-    }
 
-    //TODO: 5
+    //TODO: 5 generate new tokens & save refresh token in user document
     const options = {
       httpOnly: process.env.HTTP_ONLY === "true",
       secure: process.env.SECURE === "true",
@@ -305,34 +303,32 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
+//TODO: The fn changes password of currently logged in user
 const changeCurrentPassword = asyncHandler(async (req, res) => {
-  //TODO: 1
+  //TODO: 1 take fields
   const { oldPassword, newPassword } = req.body;
   // console.log(oldPassword, newPassword);
 
-  //TODO: 2
+  //TODO: 2 check for empty & valid fields
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
-  if (!passwordRegex.test(newPassword)) {
+  if (!passwordRegex.test(newPassword))
     throw new ApiError(
       400,
       "Password does not meet the criteria. Please use a password with at least 8 characters, including uppercase, lowercase, numbers, and special characters! ! !"
     );
-  }
 
-  //TODO: 3
+  //TODO: 3 check for existing user
   const user = await User.findById(req.user?._id);
 
-  //TODO: 4
+  //TODO: 4 verify incomming old password with custom method in user instance
   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
 
-  if (!isPasswordCorrect) {
-    throw new ApiError(400, "Invalid old password! ! !");
-  }
+  if (!isPasswordCorrect) throw new ApiError(400, "Invalid old password! ! !");
 
-  //TODO: 5
+  //TODO: 5 set new password
   user.password = newPassword;
 
-  //TODO: 6
+  //TODO: 6 save user
   await user.save({ validateBeforeSave: false });
 
   //TODO: 7
@@ -341,6 +337,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password Changed successfully!!!"));
 });
 
+//TODO: The fn returns currently logged in user
 const getCurrentUser = asyncHandler(async (req, res) => {
   //console.log("getCurrentUser", req.user);
   //TODO: 1
@@ -349,17 +346,21 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, req.user, "User found successfully!!!"));
 });
 
+//TODO: The fn updates user details with provided fields
 const updateAccountDetails = asyncHandler(async (req, res) => {
-  //TODO: 1
+  //TODO: 1 take fields
   const { fullName, email } = req.body;
-  console.log("updateAccountDetails TODO:1", fullName, email);
+  // console.log("updateAccountDetails", fullName, email);
 
-  if (!fullName || !email) {
+  //TODO: 2 check fields
+  if (!fullName || fullName.trim() === "" || !email || email.trim() === "")
     throw new ApiError(400, "Please provide full name and email! ! !");
-  }
 
-  console.log("updateAccountDetails TODO:2", req.user?._id);
-  //TODO: 2
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email.match(emailRegex))
+    throw new ApiError(400, "Invalid email address! ! !");
+
+  //TODO: 3 find & update user
   const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
@@ -371,7 +372,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     { new: true }
   ).select("-password");
 
-  //TODO: 3
+  //TODO: 4
   return res
     .status(200)
     .json(
@@ -379,29 +380,28 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     );
 });
 
+//TODO: The fn updates user avatar file & deletes old file when new file save success
 const updateUserAvatar = asyncHandler(async (req, res) => {
-  //TODO: 4.1
+  //TODO: 4.1 have old file url
   const oldAvatarCloudUrl = req.user.avatar;
   const folderPath = "chaiaurbe/users/avatars/";
 
-  //TODO: 1
+  //TODO: 1 take local path from middleware
   const avatarLocalPath = req.file?.path;
 
-  if (!avatarLocalPath) {
+  if (!avatarLocalPath)
     throw new ApiError(400, "User avatar file is missing! ! !");
-  }
 
-  //TODO: 2
+  //TODO: 2 save file on cloud
   const avatar = await uploadOnCloudinary(avatarLocalPath, "users/avatar");
 
-  if (!avatar.url) {
+  if (!avatar?.url)
     throw new ApiError(
       400,
       "Something went wrong while uploading user avatar! ! !"
     );
-  }
 
-  //TODO: 3
+  //TODO: 3 find & save user with new file url
   const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
@@ -428,9 +428,10 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Avatar updated successfully!!!"));
 });
 
+//TODO: The fn updates / set user coverimage file & deletes old file* when new file save sucess
 const updateUserCoverImage = asyncHandler(async (req, res) => {
-  //TODO: 4.1 old
-  let coverImageOldCloudUrl = "";
+  //TODO: 4.1 have old file if found
+  let coverImageOldCloudUrl = null;
   const folderPath = "chaiaurbe/users/cover-images/";
 
   //Strategy: Case: user haven't uploaded coverImage when registered, don't need to destroy coverImageOldCloudUrl.
@@ -440,27 +441,25 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     coverImageOldCloudUrl = req.user?.coverImage;
   }
 
-  //TODO: 1 new
+  //TODO: 1 take local path from middleware
   const coverImageLocalPath = req.file?.path;
 
-  if (!coverImageLocalPath) {
+  if (!coverImageLocalPath)
     throw new ApiError(400, "Cover image file is missing! ! !");
-  }
 
-  //TODO: 2
+  //TODO: 2 save on cloud
   const coverImage = await uploadOnCloudinary(
     coverImageLocalPath,
     "users/coverImage"
   );
 
-  if (!coverImage.url) {
+  if (!coverImage.url)
     throw new ApiError(
       400,
       "Something went wrong while uploading cover image! ! !"
     );
-  }
 
-  //TODO: 3
+  //TODO: 3 find & save user with new file url
   const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
@@ -471,18 +470,15 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     { new: true }
   ).select("-password");
 
-  if (!user.coverImage === coverImage.url) {
+  if (!user.coverImage === coverImage.url)
     throw new ApiError(
       500,
       "Something went wrong while updating user cover image in database! ! !"
     );
-  }
-  //? will destroy call exicute only after db updation success?
 
   //TODO: 4.2 strategy for calling destroy only if the coverImage was uploaded on registeration & cloud uploaded matches with DB updated
-  if (coverImageOldCloudUrl !== "" && user.coverImage === coverImage.url) {
+  if (!coverImageOldCloudUrl && user.coverImage === coverImage.url)
     await destroyFileOnCloudinary(folderPath, coverImageOldCloudUrl);
-  }
 
   //TODO: 5
   return res
@@ -490,6 +486,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully!!!"));
 });
 
+//TODO: pupose unclear about who is gona use to get profile, (only profile of user it self / anyone anyones profile)
 const getUserChannelProfile = asyncHandler(async (req, res) => {
   //TODO: 1
   const { username } = req.params;
@@ -565,6 +562,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+//
 const getWatchHistory = asyncHandler(async (req, res) => {
   //TODO: 1
   // console.log("getWatchHistory current user id ", typeof req.user._id),
